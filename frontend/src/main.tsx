ecf908206eeafe81924f779370e92cbc4868bf9f
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2025-03-12 02:41:33 +0530

    Integrated Socket on frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
       <App />
+      <Toaster position='bottom-right'/>
     </ClerkProvider>
   </BrowserRouter>
 );
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,41 @@
-import { UserButton } from '@clerk/clerk-react';
+import { useAuth, UserButton } from '@clerk/clerk-react';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { Socket, io } from 'socket.io-client';
 
 export const Dashboard = () => {
+  const { getToken } = useAuth();
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const connectSocket = async () => {
+      const token = await getToken();
+      const socketConnection = io(import.meta.env.VITE_WEBSOCKET_URL, {
+        auth: { token },
+        transports: ['websocket'],
+      });
+
+      socketConnection.on('connect', () => {
+        toast.success(
+          'Successfully Connected to SocketIO ' + socketConnection.id,
+          { duration: 3000 }
+        );
+      });
+
+      socketConnection.on('disconnect', (reason) => {
+        toast.error('SocketIO disconnected due to ' + reason);
+      });
+
+      setSocket(socketConnection);
+    };
+
+    connectSocket();
+
+    return () => {
+      socket?.disconnect();
+    };
+  }, [getToken]);
+
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <UserButton />
